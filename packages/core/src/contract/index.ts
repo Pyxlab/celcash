804a791d875d7fc75043ffb4af4aebee627d3b3e
Walaff Fernandes <walaffonofre@icloud.com>
2024-05-28 12:49:12 -0300

    feat: update contracts module with new exports

--- packages/core/src/contract/index.ts
+++ packages/core/src/contract/index.ts
@@ -1,41 +1,11 @@
-import { initContract } from '@ts-rest/core'
-import { z } from 'zod'
-import {
-    authorizationBodySchema,
-    authorizationResponseSchema,
-} from '../schemas/common'
-import { antecipation } from './antecipation'
-import { chargebacks } from './chargebacks'
-import { charges } from './charges'
-import { companies } from './companies'
-import { customers } from './customers'
-import { plans } from './plans'
-import { subscriptions } from './subscriptions'
-import { transactions } from './transactions'
-import { pix, transfer } from './transfer'
-
-const c = initContract()
-
-export const contract = c.router({
-    subscriptions,
-    antecipation,
-    chargebacks,
-    companies,
-    pix,
-    transfer,
-    customers,
-    plans,
-    transactions,
-    charges,
-    token: {
-        method: 'POST',
-        path: '/token',
-        responses: {
-            200: authorizationResponseSchema,
-        },
-        body: authorizationBodySchema,
-        headers: z.object({
-            Authorization: z.string().regex(/^Basic .+$/),
-        }),
-    },
-})
+export { antecipation } from './antecipation'
+export { auth } from './auth'
+export { cards } from './cards'
+export { chargebacks } from './chargebacks'
+export { charges } from './charges'
+export { companies } from './companies'
+export { customers } from './customers'
+export { plans } from './plans'
+export { subscriptions } from './subscriptions'
+export { transactions } from './transactions'
+export { pix, transfer } from './transfer'
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
-export * from './contract'
 export type {
     AuthorizationBody,
     AuthorizationResponse,
