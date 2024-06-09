26092f4e5a4060ce480a74fe999c6c06b10d8049
Walaff Fernandes <walaffonofre@icloud.com>
2024-06-09 03:31:35 -0300

    refactor: update types in types.ts

---
+++ packages/adonisjs/src/types.ts
@@ -0,0 +1,29 @@
+/**
+ * @cel_cash/adonisjs
+ *
+ * @license MIT
+ * @copyright Walaff Fernandes <walaffonofre@gmail.com>
+ */
+
+export interface CelCashConfig {
+  /**
+   * The base URL of the CelCash API
+   */
+  baseUrl: string
+
+  /**
+   * The API key to use for requests
+   */
+  id: string
+
+  /**
+   * The API secret to use for requests
+   */
+  hash: string
+}
+
+export interface ResolvedConfig {
+  baseUrl: string
+  id: string
+  hash: string
+}
