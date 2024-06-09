fc382cc3479ac8c6836f60e59464ca22e92ec0e3
Walaff Fernandes <walaffonofre@icloud.com>
2024-06-09 03:32:04 -0300

    refactor: add defineConfig function to @cel_cash/adonisjs package

---
+++ packages/adonisjs/src/define_config.ts
@@ -0,0 +1,28 @@
+/**
+ * @cel_cash/adonisjs
+ *
+ * @license MIT
+ * @copyright Walaff Fernandes <walaffonofre@gmail.com>
+ */
+
+import { InvalidArgumentsException } from '@adonisjs/core/exceptions'
+import type { CelCashConfig } from './types.js'
+
+/**
+ * Define lock configuration
+ */
+export function defineConfig(config: CelCashConfig) {
+  if (!config.baseUrl) {
+    throw new InvalidArgumentsException('Missing "baseUrl" property for CelCash config')
+  }
+
+  if (!config.id) {
+    throw new InvalidArgumentsException('Missing "id" property for CelCash config')
+  }
+
+  if (!config.hash) {
+    throw new InvalidArgumentsException('Missing "hash" property for CelCash config')
+  }
+
+  return config
+}
