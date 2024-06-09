096ff8068b7789ce226c8d8dea89fdc94400ea11
Walaff Fernandes <walaffonofre@icloud.com>
2024-06-09 16:47:02 -0300

    feat: add endpoint for listing plans

--- apps/adonis-example/start/routes.ts
+++ apps/adonis-example/start/routes.ts
@@ -8,9 +8,27 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import cel_cash from '@cel_cash/adonisjs/services/main'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
+})
+
+router.get('/plans', async ({ request, response }) => {
+  const { limit = 10, startAt = 0 } = request.qs()
+
+  const result = await cel_cash.plans.list({
+    query: {
+      limit: +limit,
+      startAt: +startAt,
+    },
+  })
+
+  if (result.status !== 200) {
+    return response.status(result.status).json(result.body)
+  }
+
+  return result.body.Plans
 })
