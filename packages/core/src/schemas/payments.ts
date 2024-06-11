84294d318fd62c6beeccf4a78f0dcdfc27a74506
Walaff Fernandes <walaffonofre@icloud.com>
2024-06-11 09:19:40 -0300

    feat: refactor paymentMethodCreditCardSchema validation logic

--- packages/core/src/schemas/payments.ts
+++ packages/core/src/schemas/payments.ts
@@ -1,3 +1,4 @@
+import { P, match } from 'ts-pattern'
 import { z } from 'zod'
 import {
     agreementSchema,
@@ -7,15 +8,43 @@ import {
 import { cardSchema } from './cards.js'
 
 export const paymentMethodCreditCardSchema = z.object({
-    Card: cardSchema
-        .deepPartial()
-        .refine(({ galaxPayId, myId, number, holder, expiresAt, cvv }) => {
-            if (!galaxPayId) {
-                return !!myId && !!number && !!holder && !!expiresAt && !!cvv
-            }
-
-            return true
-        }, 'You must provide all fields if you are not providing the galaxPayId'),
+    Card: cardSchema.deepPartial().refine(values => {
+        return match(values)
+            .with(
+                {
+                    myId: P.string,
+                    galaxPayId: P.nullish,
+                    number: P.nullish,
+                    holder: P.nullish,
+                    expiresAt: P.nullish,
+                    cvv: P.nullish,
+                },
+                () => true,
+            )
+            .with(
+                {
+                    galaxPayId: P.number,
+                    myId: P.nullish,
+                    number: P.nullish,
+                    holder: P.nullish,
+                    expiresAt: P.nullish,
+                    cvv: P.nullish,
+                },
+                () => true,
+            )
+            .with(
+                {
+                    galaxPayId: P.nullish,
+                    myId: P.string,
+                    number: P.string,
+                    holder: P.string,
+                    expiresAt: P.string,
+                    cvv: P.string,
+                },
+                () => true,
+            )
+            .otherwise(() => false)
+    }, 'You must provide all fields if you are not providing the galaxPayId or myId'),
     Antifraud: z
         .object({
             ip: z.string().ip(),
