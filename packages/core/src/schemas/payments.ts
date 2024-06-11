ec1912b7823c168d925d4fcd9f000f9c483bc977
Walaff Fernandes <walaffonofre@icloud.com>
2024-06-11 10:28:27 -0300

    fix: update paymentMethodCreditCardSchema validation logic
    The paymentMethodCreditCardSchema validation logic has been refactored to remove unnecessary code and improve readability.

--- packages/core/src/schemas/payments.ts
+++ packages/core/src/schemas/payments.ts
@@ -1,4 +1,4 @@
-import { P, match } from 'ts-pattern'
+// import { P, match } from 'ts-pattern'
 import { z } from 'zod'
 import {
     agreementSchema,
@@ -8,43 +8,44 @@ import {
 import { cardSchema } from './cards.js'
 
 export const paymentMethodCreditCardSchema = z.object({
-    Card: cardSchema.deepPartial().refine(values => {
-        return match(values)
-            .with(
-                {
-                    myId: P.string,
-                    galaxPayId: P.nullish,
-                    number: P.nullish,
-                    holder: P.nullish,
-                    expiresAt: P.nullish,
-                    cvv: P.nullish,
-                },
-                () => true,
-            )
-            .with(
-                {
-                    galaxPayId: P.number,
-                    myId: P.nullish,
-                    number: P.nullish,
-                    holder: P.nullish,
-                    expiresAt: P.nullish,
-                    cvv: P.nullish,
-                },
-                () => true,
-            )
-            .with(
-                {
-                    galaxPayId: P.nullish,
-                    myId: P.string,
-                    number: P.string,
-                    holder: P.string,
-                    expiresAt: P.string,
-                    cvv: P.string,
-                },
-                () => true,
-            )
-            .otherwise(() => false)
-    }, 'You must provide all fields if you are not providing the galaxPayId or myId'),
+    Card: cardSchema.deepPartial(),
+    // .refine(values => {
+    //     return match(values)
+    //         .with(
+    //             {
+    //                 myId: P.string,
+    //                 galaxPayId: P.nullish,
+    //                 number: P.nullish,
+    //                 holder: P.nullish,
+    //                 expiresAt: P.nullish,
+    //                 cvv: P.nullish,
+    //             },
+    //             () => true,
+    //         )
+    //         .with(
+    //             {
+    //                 galaxPayId: P.number,
+    //                 myId: P.nullish,
+    //                 number: P.nullish,
+    //                 holder: P.nullish,
+    //                 expiresAt: P.nullish,
+    //                 cvv: P.nullish,
+    //             },
+    //             () => true,
+    //         )
+    //         .with(
+    //             {
+    //                 galaxPayId: P.nullish,
+    //                 myId: P.string,
+    //                 number: P.string,
+    //                 holder: P.string,
+    //                 expiresAt: P.string,
+    //                 cvv: P.string,
+    //             },
+    //             () => true,
+    //         )
+    //         .otherwise(() => false)
+    // }, 'You must provide all fields if you are not providing the galaxPayId or myId')
     Antifraud: z
         .object({
             ip: z.string().ip(),
